cfe37ac4d9f068bc502fa2ccbd94d52090ce55c8
Neeraj Singh <137694450+Neeraj99990@users.noreply.github.com>
2024-01-31 19:53:56 +0530

    First code

---
+++ Code1.java
@@ -0,0 +1,13 @@
+public class Code1 {
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        if (num > 0)
+            System.out.println("Number is positive");
+        else if ( num < 0)
+            System.out.println("Number is negetive");
+            else
+                System.out.println("Zero");
+     }
+}
