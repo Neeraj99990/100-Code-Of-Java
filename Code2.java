b98546f8ee14b6011e507c0e35f7672941759c88
Niraj Singh <137694450+Neeraj99990@users.noreply.github.com>
2024-02-01 16:44:43 +0530

    Update Code2.java

--- Code2.java
+++ Code2.java
@@ -1,2 +1,12 @@
+// Chek the NUmber is Even Or Odd
+
 public class Code2 {
+    public static void main(String[] args) {
+              int number= 29;
+
+              if ( number % 2 == 0)
+                  System.out.println( number + "  Number is Even" );
+              else
+                  System.out.println( number + "  Number is Odd ");
+    }
 }
