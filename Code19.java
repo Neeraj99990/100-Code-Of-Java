4412d7be5b54dddfaea36007d854a3c8a8af2495
Neeraj Singh <137694450+Neeraj99990@users.noreply.github.com>
2024-02-10 16:15:34 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Code19.java
@@ -0,0 +1,17 @@
+// Perfect Number Program in Java
+public class Code19 {
+    public static void main(String[] args) {
+        int n = 28, sum = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (n % i == 0)
+                sum = sum + i;
+        }
+
+        if (sum == n)
+            System.out.println (n + " Is a perfect number");
+        else
+            System.out.println (n + " Is not a perfect number");
+    }
+}
