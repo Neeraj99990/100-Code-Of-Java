25948ab0d9d835aac95ea4846de4e76d32151e22
Neeraj Singh <137694450+Neeraj99990@users.noreply.github.com>
2024-02-21 17:17:42 +0530

    ALL WORK Is done

---
+++ Code34.java
@@ -0,0 +1,15 @@
+
+// Java Program To calculate the Area Of Circle 
+
+/**
+ * Code34
+ */
+public class Code34 {
+
+    public static void main(String[] args) {
+        double radius = 7;
+        // formula of area of circle
+        double area = 3.14 * radius * radius;
+        System.out.println(area);
+    }
+}
