a23f528588a1c77c1cdf489b039bb049b0eb50cd
Neeraj Singh <137694450+Neeraj99990@users.noreply.github.com>
2024-03-07 18:28:49 +0530

    commit

---
+++ Code46.java
@@ -0,0 +1,20 @@
+// java find the largest and smallest element in java 
+public class Code46 {
+    public static void main(String args[]) {
+
+        int arr[] = { 12, 13, 1, 10, 34, 10 };
+
+        int largest = arr[0], smallest = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (smallest > arr[i])
+                smallest = arr[i];
+            if (largest < arr[i])
+                largest = arr[i];
+
+        }
+
+        System.out.println(smallest);
+        System.out.println(largest);
+    }
+}
