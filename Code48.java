06d5ada18dec214b042e680f064e7d8ab633a85a
Neeraj Singh <137694450+Neeraj99990@users.noreply.github.com>
2024-03-07 18:43:23 +0530

    all done

---
+++ Code48.java
@@ -0,0 +1,17 @@
+// To reverse an array ina java 
+/**
+ * Code48
+ */
+public class Code48 {
+
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 30, 40, 50 };
+
+        int n = arr.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+
+        }
+    }
+}
