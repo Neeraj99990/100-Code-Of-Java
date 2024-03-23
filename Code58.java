179c59e005c41831b1200227abeab3403e52ef76
Neeraj Singh <137694450+Neeraj99990@users.noreply.github.com>
2024-03-23 10:23:53 +0530

    all done

---
+++ Code58.java
@@ -0,0 +1,15 @@
+// java program to print ASII values of a character 
+
+/**
+ * Code58
+ */
+public class Code58 {
+
+    public static void main(String[] args) {
+        char c = 'A';
+
+        int i = c;
+
+        System.out.println("ASCII value of " + c + "is " + i);
+    }
+}
