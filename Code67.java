791d46d3c9e286545ddf6b8c6f1437065ddf99f7
Neeraj Singh <137694450+Neeraj99990@users.noreply.github.com>
2024-04-01 17:36:09 +0530

    print alphabets

---
+++ Code67.java
@@ -0,0 +1,17 @@
+public class Code67 {
+    public static void main(String[] args) {
+        // Print uppercase alphabets
+        System.out.println("Uppercase Alphabets:");
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.print(ch + " ");
+        }
+
+        System.out.println(); // Print a new line
+
+        // Print lowercase alphabets
+        System.out.println("\nLowercase Alphabets:");
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            System.out.print(ch + " ");
+        }
+    }
+}
