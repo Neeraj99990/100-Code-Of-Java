76a8eb120ac653d5fb1ce14accede9f8a0f173f8
Neeraj Singh <137694450+Neeraj99990@users.noreply.github.com>
2024-04-29 20:50:57 +0530

    all Done

---
+++ Code82.java
@@ -0,0 +1,9 @@
+class Code82 {
+    public static void main(String[] args) {
+
+        // get the name of operating system
+        String operatingSystem = System.getProperty("os.name");
+        System.out.println(operatingSystem);
+
+    }
+}
