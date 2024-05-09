d4b26a55b23b52ece7b007d4644111cf73f1fb96
Niraj Singh <neerajsingh009424@gmail.com>
2024-05-09 17:31:15 +0530

    all set

---
+++ Code90.java
@@ -0,0 +1,10 @@
+public class Code90 {
+    public static void main(String[] args) {
+        System.out.println("java Version: " + System.getProperty("java.version"));
+        System.out.println("java Runtime Version :" + System.getProperty("java.runtime.version"));
+        System.out.println("java Home: " + System.getProperty("java.home"));
+        System.out.println("Java Vendor: " + System.getProperty("java.vendor"));
+
+
+    }
+}
