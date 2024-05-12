b51153c5f087e613dca4df49b45069a2c480924e
Niraj Singh <neerajsingh009424@gmail.com>
2024-05-12 17:59:44 +0530

    all set

---
+++ Code95.java
@@ -0,0 +1,24 @@
+/**
+ * Code95
+ */
+public class Code95 {
+
+    private static void LinearSearch(int[] arr, int item)
+    {
+        for ( int i = 0; i < arr.length; i++)
+        {
+               if (arr[i] == item )
+               {
+                System.out.println(item + " Found at index : " +i);
+                return;
+               }
+        }
+        System.out.println("Not found ");
+    } 
+    public static void main(String[] args) {
+        int[] arr = {12,5,18,25,-3,19};
+
+        int item = 19;
+        LinearSearch(arr, item);
+    }
+}
